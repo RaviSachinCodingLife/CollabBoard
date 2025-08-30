b14102aacf87d4c3a0e524e2d673afa0d956491f
RaviSachinCodingLife <sachinsaurabh.msbcgroup@gmail.com>
2025-08-30 20:55:56 +0530

    changed in code

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 const token = localStorage.getItem("token");
+
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  path: "/socket.io", 
   transports: ["websocket"],
   withCredentials: true,
   auth: { token },
